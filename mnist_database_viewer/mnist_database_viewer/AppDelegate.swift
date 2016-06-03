a901ae15bf47a11ae4cabeedbdbf8bdff169d63a
Jeroen Torfs <waveshaper@telenet.be>
2016-06-03 19:51:50 +0200

    commit swift project

---
+++ mnist_database_viewer/mnist_database_viewer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  mnist_database_viewer
+//
+//  Created by Jeroen Torfs on 03/06/16.
+//  Copyright © 2016 Jeroen Torfs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
